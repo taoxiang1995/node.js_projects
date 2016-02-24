2c14d30bbc0517ddc46d44bf169d6c6e3a0ed3e8
fumu <332853786@qq.com>
2016-02-24 14:42:38 -0800

    change restriction:

--- routes/api.js
+++ routes/api.js
@@ -61,7 +61,7 @@ api.post('/join', function(req, res){
 		  //build the array for nearby 5 locations
 		  var location = [];
 
-		  for (var i = 0; i<5 && google_rest_result.results[i] != null; i++)
+		  for (var i = 0; i<5 && google_rest_result.results.length; i++)
 		  {
 		    var latitude = google_rest_result.results[i].geometry.location.lat;
 		    var longitude =google_rest_result.results[i].geometry.location.lng;
